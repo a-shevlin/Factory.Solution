a752a52d54698606771dc5e10aed72585ce40651
Alex Shevlin <alexshevlin1@gmail.com>
2022-08-05 10:46:33 -0700

    fix context name to reflect project name.

--- Factory/Models/DesignTimeDbContectFactory.cs
+++ Factory/Models/DesignTimeDbContectFactory.cs
@@ -5,21 +5,21 @@ using System.IO;
 
 namespace Factory.Models
 {
-  public class FactoryContextFactory : IDesignTimeDbContextFactory<DojoContext>
+  public class FactoryContextFactory : IDesignTimeDbContextFactory<FactoryContext>
   {
 
-    DojoContext IDesignTimeDbContextFactory<DojoContext>.CreateDbContext(string[] args)
+    FactoryContext IDesignTimeDbContextFactory<FactoryContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
           .AddJsonFile("appsettings.json")
           .Build();
 
-      var builder = new DbContextOptionsBuilder<DojoContext>();
+      var builder = new DbContextOptionsBuilder<FactoryContext>();
 
       builder.UseMySql(configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(configuration["ConnectionStrings:DefaultConnection"]));
 
-      return new DojoContext(builder.Options);
+      return new FactoryContext(builder.Options);
     }
   }
 }
