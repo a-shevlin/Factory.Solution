11d6842f8212b7f15d3fead16a4d29033c1654a2
Alex Shevlin <alexshevlin1@gmail.com>
2022-08-06 23:25:46 -0700

    add ViewBags for All Engineers and All Machines.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
 
 namespace DoctorsOffice.Controllers
 {
   public class HomeController : Controller
   {
+
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
       ViewBag.PageTitle = ("Dr. Sillystringz's Factory");
       ViewBag.Header = ("Welcome to the Factory!");
+      ViewBag.AllEngineers = _db.Engineers.ToList();
+      ViewBag.AllMachines = _db.Machines.ToList();
       return View();
     }
   }
